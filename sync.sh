b6ee3096fb22ea60eabf422ae06b61dfd4a9f6c1
David Koller <david.koller@xmv-solutions.com>
2016-06-12 20:49:23 +0200

    update

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ ServerURL=`cat /ownCloudVolume/ownCloudConf/ServerURL`
 export ServerURL
 
 owncloudcmd --silent --non-interactive -n --exclude /ownCloudVolume/ownCloudConf/sync-exclude.lst /ownCloudVolume/ownCloudData ${ServerURL} \
-	> ownCloudVolume/ownCloudLog/csync.log.\`date -Iseconds\` 2>&1
+	> ownCloudVolume/ownCloudLog/csync.log.`date -Iseconds` 2>&1
