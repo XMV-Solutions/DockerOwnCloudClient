e0356ae4ec1a255e0c64946b55b00c2a6a04cec7
David Koller <david.koller@xmv-solutions.com>
2016-06-12 20:25:53 +0200

    added extra syncCommand

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM alpine:edge
 MAINTAINER David Koller <david.koller@xmv-solutions.com>
 
 # Install ownCloud-client
-RUN echo -e '@edge http://dl-cdn.alpinelinux.org/alpine/edge/main\n@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories && \
+ RUN echo -e '@edge http://dl-cdn.alpinelinux.org/alpine/edge/main\n@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories && \
     apk add --no-cache owncloud-client@testing
 
 # All in One Volume
--- createSyncCommand.sh
+++ createSyncCommand.sh
@@ -1,43 +1,52 @@
 #!/bin/sh
 
 # CreateConfFiles & ConfVars
-mkdir -p /ownCloudVolume/.owncloudConf/
+mkdir -p /ownCloudVolume/.ownCloudConf/
 mkdir -p /ownCloudVolume/ownCloudData/
 mkdir -p /ownCloudVolume/.ownCloudLog/
 
 # ServerAddress
-if [ ! -f /ownCloudVolume/.owncloudConf/ServerURL ] ; then
- echo "https://owncloud." > /ownCloudVolume/.owncloudConf/ServerURL
+if [ ! -f /ownCloudVolume/.ownCloudConf/ServerURL ] ; then
+ echo "https://owncloud." > /ownCloudVolume/.ownCloudConf/ServerURL
 fi
-ServerURL=`cat /ownCloudVolume/.owncloudConf/ServerURL`
+ServerURL=`cat /ownCloudVolume/.ownCloudConf/ServerURL`
+export ServerURL
 
 # SyncPeriod
-if [ ! -f /ownCloudVolume/.owncloudConf/SyncCronPeriod ] ; then
- echo "*/5 * * * *" > touch /ownCloudVolume/.owncloudConf/SyncCronPeriod
+if [ ! -f /ownCloudVolume/.ownCloudConf/SyncCronPeriod ] ; then
+	# Standard is one check every 5 minutes
+ 	echo "*/5 * * * *" > /ownCloudVolume/.ownCloudConf/SyncCronPeriod
 fi
-SyncCronPeriod=`cat /ownCloudVolume/.owncloudConf/SyncCronPeriod`
+SyncCronPeriod=`cat /ownCloudVolume/.ownCloudConf/SyncCronPeriod`
+export SyncCronPeriod
 
 # Credentials
-if [ ! -f /ownCloudVolume/.owncloudConf/netrcStyleConf ] ; then
- echo "default login [yourOwncloudUserName] password [yourOwncloudUserPassword]" > touch /ownCloudVolume/.owncloudConf/netrcStyleConf
+if [ ! -f /ownCloudVolume/.ownCloudConf/netrcStyleConf ] ; then
+ echo "default login [yourOwncloudUserName] password [yourOwncloudUserPassword]" > /ownCloudVolume/.ownCloudConf/netrcStyleConf
 fi
 if [ ! -f ~/.netrc ] ; then
-	ln -s /ownCloudVolume/.owncloudConf/netrcStyleConf ~/.netrc
+	ln -s /ownCloudVolume/.ownCloudConf/netrcStyleConf ~/.netrc
 fi
 
 # /sync-exclude.lst 
-if [ ! -f /ownCloudVolume/.owncloudConf/sync-exclude.lst  ] ; then
- cp /sync-exclude.lst /ownCloudVolume/.owncloudConf/sync-exclude.lst
+if [ ! -f /ownCloudVolume/.ownCloudConf/sync-exclude.lst  ] ; then
+ cp /sync-exclude.lst /ownCloudVolume/.ownCloudConf/sync-exclude.lst
 fi
 
+echo -en "SyncCronPeriod: ${SyncCronPeriod} "
+echo -en "ServerURL: ${ServerURL} "
+echo "netrcStyleConf: $(cat ~/.netrc) "
 
-echo "ServerURL: ${ServerURL}"
-echo "SyncCronPeriod: ${SyncCronPeriod}"
-echo "netrcStyleConf: $(cat ~/.netrc)"
+owncloudcmd --silent --non-interactive -n --exclude /ownCloudVolume/.ownCloudConf/sync-exclude.lst /ownCloudVolume/ownCloudData ${ServerURL} \
+	> ownCloudVolume/.ownCloudLog/csync.log.\`date -Iseconds\` 2>&1
 
 # Install SyncCommand in CronTab
-echo "${SyncCronPeriod} owncloudcmd --silent --non-interactive -n --exclude /ownCloudVolume/.owncloudConf/sync-exclude.lst /ownCloudVolume/ownCloudData ${ServerURL} \
-	> ownCloudVolume/.ownCloudLog/csync.log.\`date -Iseconds\` 2>&1" \
-	| crontab -
+echo "${SyncCronPeriod} /createSyncCommand.sh" | crontab -
 
-crond -f -d 8
+# Run crond once
+if ps ax | grep cro | grep nd 2>&1 >/dev/null ; then 
+	let x++	
+else
+	x=1 
+	crond -f -d 8
+fi
