d95c8d374f75326c1c3e65d0cd71c78704ab5054
David Koller <david.koller@xmv-solutions.com>
2016-06-20 12:54:27 +0200

    Only one sync at a time

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 ServerURL=`cat /ownCloudVolume/ownCloudConf/ServerURL`
 export ServerURL
 
-if ps ax | grep owncl | grep loudcmd -q ; then
+if ps ax | grep sync.s | grep ync.sh -q ; then
 	echo "already running sync"
 else
 	owncloudcmd --silent --non-interactive -n --exclude /ownCloudVolume/ownCloudConf/sync-exclude.lst /ownCloudVolume/ownCloudData ${ServerURL} \
