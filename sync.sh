c1ec7496769fc50eedea945ef7767c58104906be
David Koller <david.koller@xmv-solutions.com>
2016-06-25 17:41:06 +0200

    changed log

--- sync.sh
+++ sync.sh
@@ -6,12 +6,12 @@ import_config
 if ps ax | grep owncl | grep oudcmd -q ; then
 	echo "already running sync"
 else
-	echo -en "starting sync at $(date -Iseconds) --> "
+	echo -en "starting sync at $(date -Iseconds) - "
 
 	owncloudcmd --silent --non-interactive -n --exclude $ocEXCLUDE $ocDATADIR $ocServerURL \
 		> $ocLOGDIR/csync.log.`date -Iseconds` 2>&1
 
-	echo "sync finished"
+	echo "sync finished $(date -Iseconds)"
 fi
 
 exit 0
