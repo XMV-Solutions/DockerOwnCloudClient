61795eeb56954fb6ac6118ceba97059e1eaf439f
David Koller <david.koller@xmv-solutions.com>
2016-06-25 18:30:48 +0200

    changed logging

--- sync.sh
+++ sync.sh
@@ -7,9 +7,21 @@ if ps ax | grep owncl | grep oudcmd -q ; then
 	echo "already running sync"
 else
 	echo -en "starting sync at $(date -Iseconds) - "
-
+	
+	today=$(date -I)
+	yesterday=$(date -I -d @"$(( $(date +%s) - 86400 ))" )
+	
+	# Backup yesterdays logs, make logfolder for today
+	if [ ! -f $ocLOGDIR/$today ] ; then
+		mkdir -p $ocLOGDIR/$today
+		if [ -f $ocLOGDIR/${yesterday} ] ; then
+			tar cjf  $ocLOGDIR/${yesterday}.tbz  $ocLOGDIR/${yesterday} 2>&1 > /dev/null || echo "No logs saved from yesterday"
+			rm -rf  $ocLOGDIR/${yesterday} 2>&1 > /dev/null || echo "No logs removed from yesterday"
+		fi
+	fi
+	
 	owncloudcmd --silent --non-interactive -n --exclude $ocEXCLUDE $ocDATADIR $ocServerURL \
-		> $ocLOGDIR/csync.log.`date -Iseconds` 2>&1
+		> $ocLOGDIR/$today/csync.log.`date -Iseconds` 2>&1
 
 	echo "sync finished $(date -Iseconds)"
 fi
